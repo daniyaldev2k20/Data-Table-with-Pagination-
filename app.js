2a2fa09a58a13bf470cce46cb1a322d7bb90d10e
daniyaldev2k20 <daniyal.fa12bcs@gmail.com>
2021-03-06 19:32:44 +0500

    sixth commit

--- app.js
+++ app.js
@@ -12,14 +12,7 @@ app.use(cors());
 
 app.options('*', cors());
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+app.use(express.static('build'));
 
 const limiter = rateLimit({
     max: 100, //max number of requests
